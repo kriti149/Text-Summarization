c1beddd1874d986763ed75c4256e25f8c280f87a
Kriti Gupta <35222739+kriti149@users.noreply.github.com>
2019-06-01 12:57:51 +0530

    Add files via upload

---
+++ txt_summarization.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 25 16:58:06 2018
+
+@author: Kriti Gupta
+"""
+import bs4 as bs
+import urllib.request
+import re
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+import heapq
+
+source=urllib.request.urlopen('https://en.wikipedia.org/wiki/Global_warming').read()
+soup=bs.BeautifulSoup(source,'lxml')
+text=""
+for paragraph in soup.find_all('p'):
+     text +=paragraph.text
+text=re.sub(r'\[[0-9]*\]',' ',text)
+text=re.sub(r'\s+',' ',text)
+clean_text=text.lower()
+clean_text=re.sub(r'\W',' ',clean_text)
+clean_text=re.sub(r'\d',' ',clean_text)
+clean_text=re.sub(r'\s+',' ',clean_text)
+
+sentences=nltk.sent_tokenize(text)
+stop_words=nltk.corpus.stopwords.words('english')
+word2count={}
+for word in nltk.word_tokenize(clean_text):
+    if word not in stop_words:
+        if word not in word2count.keys():
+            word2count[word]=1
+        else:
+            word2count[word]+=1
+
+for word in word2count.keys():
+    word2count[word]=word2count[word]/max(word2count.values())
+sent2score={}
+for sentence in sentences:
+    for word in nltk.word_tokenize(sentence.lower()):
+        if word in word2count.keys():
+            if len(sentence.split(' '))<25:
+                if sentence not in sent2score.keys():
+                    sent2score[sentence]=word2count[word]
+                else:
+                    sent2score[sentence]+=word2count[word]
+
+best_sentences=heapq.nlargest(5,sent2score,key=sent2score.get)
+print('------------------------------------------------------------')
+for sentence in best_sentences:
+    print(sentence)
+
+                    
